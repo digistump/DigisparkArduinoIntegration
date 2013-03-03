9acc1fbbc645797fb0cb857cbe847c095a7f5ec8
Bluebie <a@creativepony.com>
2013-03-03 18:08:14 +1100

    digispark tiny core: modded wiring.c timer stuff to be more accurate on tiny85's running at different clock speeds (including 16.5mhz!!!)

--- hardware/tiny-digispark/cores/tiny/wiring.c
+++ hardware/tiny-digispark/cores/tiny/wiring.c
@@ -77,7 +77,8 @@ volatile unsigned long millis_timer_overflow_count = 0;
 volatile unsigned long millis_timer_millis = 0;
 static unsigned char millis_timer_fract = 0;
 
-ISR(MILLISTIMER_OVF_vect)
+// bluebie changed isr to noblock so it wouldn't mess up USB libraries
+ISR(MILLISTIMER_OVF_vect, ISR_NOBLOCK)
 {
   // copy these to local variables so they can be stored in registers
   // (volatile variables must be read from memory on every access)
@@ -95,7 +96,7 @@ ISR(MILLISTIMER_OVF_vect)
 ...rmv */
 
   f += FRACT_INC;
-
+  
   if (f >= FRACT_MAX) 
   {
     f -= FRACT_MAX;
@@ -124,11 +125,6 @@ unsigned long millis()
 
   return m;
 }
-
-// (unsigned long) internalMicrosProvider = NULL;
-// void registerMicrosProvider((unsigned long)function*(void)) {
-//   
-// }
 
 unsigned long micros() 
 {
@@ -159,55 +155,28 @@ void delay(unsigned long ms)
   }
 }
 
-/* Delay for the given number of microseconds.  Assumes a 8 or 16 MHz clock. */
-void delayMicroseconds(unsigned int us)
-{
-  // calling avrlib's delay_us() function with low values (e.g. 1 or
-  // 2 microseconds) gives delays longer than desired.
-  //delay_us(us);
-
-#if F_CPU >= 16000000L
-  // for the 16 MHz clock on most Arduino boards
-
-  // for a one-microsecond delay, simply return.  the overhead
-  // of the function call yields a delay of approximately 1 1/8 us.
-  if (--us == 0)
-    return;
-
-  // the following loop takes a quarter of a microsecond (4 cycles)
-  // per iteration, so execute it four times for each microsecond of
-  // delay requested.
-  us <<= 2;
-
-  // account for the time taken in the preceeding commands.
-  us -= 2;
+// special optimised delay loops for attiny85 by bluebie
+#if F_CPU == 16500000
+	// special version to deal with half-mhz speed. in a resolution of 2us increments, rounded up
+	// this loop has been tuned empirically with an oscilloscope and works in avr-gcc 4.5.1
+	void delayMicroseconds(unsigned int microseconds)
+	{
+    microseconds &= ((unsigned int) 0) - ((unsigned int) 2); // remove least signifficant bit
+		while (microseconds > 1) {
+			// 16 nops
+			asm("NOP");asm("NOP");asm("NOP");asm("NOP");asm("NOP");asm("NOP");asm("NOP");asm("NOP");
+			asm("NOP");asm("NOP");asm("NOP");asm("NOP");asm("NOP");asm("NOP");asm("NOP");asm("NOP");
+			// 16 nops
+			asm("NOP");asm("NOP");asm("NOP");asm("NOP");asm("NOP");asm("NOP");asm("NOP");asm("NOP");
+			asm("NOP");asm("NOP");asm("NOP");
+		
+		
+			microseconds -= 2;
+		}
+	}
 #else
-  // for the 8 MHz internal clock on the ATmega168
-
-  // for a one- or two-microsecond delay, simply return.  the overhead of
-  // the function calls takes more than two microseconds.  can't just
-  // subtract two, since us is unsigned; we'd overflow.
-  if (--us == 0)
-    return;
-  if (--us == 0)
-    return;
-
-  // the following loop takes half of a microsecond (4 cycles)
-  // per iteration, so execute it twice for each microsecond of
-  // delay requested.
-  us <<= 1;
-    
-  // partially compensate for the time taken by the preceeding commands.
-  // we can't subtract any more than this or we'd overflow w/ small delays.
-  us--;
+	#define delayMicroseconds(microseconds) _delay_loop_2((((unsigned int)microseconds) * (F_CPU / 100000)) / 40)
 #endif
-
-  // busy wait
-  __asm__ __volatile__ (
-    "1: sbiw %0,1" "\n\t" // 2 cycles
-    "brne 1b" : "=w" (us) : "0" (us) // 2 cycles
-  );
-}
 
 static void initToneTimerInternal(void)
 {
