c934cb8d7bf07513ab089ab67c90fd286a461086
Bluebie <a@creativepony.com>
2013-03-05 11:40:41 +1100

    Improved style of DigiMouse example and switched it to using DigiMouse.delay for timing - clarity for beginners is much better now.

--- libraries/DigisparkMouse/examples/Mouse/Mouse.ino
+++ libraries/DigisparkMouse/examples/Mouse/Mouse.ino
@@ -1,33 +1,31 @@
-//DigiMouse test and usage documentation
-//CAUTION!!!! This does click things!!!!!!!!
-//Created by Sean Murphy (duckythescientist)
+// DigiMouse test and usage documentation
+// CAUTION!!!! This does click things!!!!!!!!
+// Created by Sean Murphy (duckythescientist)
 
 #include "DigiMouse.h"
 
-unsigned int count = 0;
-
 void setup() {
-  //Do nothing? It seems as if the USB hardware is ready to go on reset
-
+  // Do nothing? It seems as if the USB hardware is ready to go on reset
 }
 
 void loop() {
+  // If not using plentiful DigiMouse.delay(), make sure to call
+  // DigiMouse.update() at least every 50ms
   
-  DigiMouse.update();//call this at least every 50ms
-  //calling more often than that is fine
-  //this will actually only send the data every once in a while unless the data is different
+  // move across the screen
+  // these are signed chars
+  DigiMouse.moveY(10); //down 10
+  DigiMouse.delay(500);
+  DigiMouse.moveX(20); //right 20
+  DigiMouse.delay(500);
+  DigiMouse.scroll(5);
+  DigiMouse.delay(500);
   
-  //move across the screen
-  //these are signed chars
-  if(!count--) DigiMouse.moveY(10);//down 10
-  if(count == 30000) DigiMouse.moveX(20);//right 20
-  if(count == 50000) DigiMouse.scroll(5);//scroll down 5
+  // or DigiMouse.move(X, Y, scroll) works
   
-  //or DigiMouse.move(X, Y, scroll) works
-  
-  //three buttons are the three LSBs of an unsigned char
-  if(count == 29000) DigiMouse.setButtons(1<<0);//left click
-  if(count == 31000) DigiMouse.setButtons(0);//unclick all
-  
-
+  // three buttons are the three LSBs of an unsigned char
+  DigiMouse.setButtons(1<<0); //left click
+  DigiMouse.delay(500);
+  DigiMouse.setButtons(0); //unclick all
+  DigiMouse.delay(500);
 }
