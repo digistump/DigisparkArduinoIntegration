f55f13fc506bcdba78b058df100909834106dc36
Bluebie <a@creativepony.com>
2013-03-05 12:04:29 +1100

    DigiJoystick: updated example to be clearer and use DigiJoystick.delay()

--- libraries/DigisparkJoystick/examples/Joystick/Joystick.ino
+++ libraries/DigisparkJoystick/examples/Joystick/Joystick.ino
@@ -1,40 +1,38 @@
 //DigiJoystick test and usage documentation
 
 #include "DigiJoystick.h"
-char myBuf[8] = {0};
-
-unsigned int count = 0;
 
 void setup() {
-  //Do nothing? It seems as if the USB hardware is ready to go on reset
-
+  // Do nothing? It seems as if the USB hardware is ready to go on reset
 }
 
 
 void loop() {
+  // If not using plentiful DigiJoystick.delay() calls, make sure to
+  //DigiJoystick.update(); // call this at least every 50ms
+  // calling more often than that is fine
+  // this will actually only send the data every once in a while unless the data is different
   
-  DigiJoystick.update();//call this at least every 50ms
-  //calling more often than that is fine
-  //this will actually only send the data every once in a while unless the data is different
+  // you can set the values from a raw byte array with:
+  // char myBuf[8] = {
+  //   x, y, xrot, yrot, zrot, slider,
+  //   buttonLowByte, buttonHighByte
+  // };
+  // DigiJoystick.setValues(myBuf);
   
-  //toggle button 1
-  if(!count--) myBuf[6] = !myBuf[6];//about 2Hz blinking
-  //this means looping happens at about 65536 times per second
-  //15uS per loop
+  // Or we can also set values like this:
+  DigiJoystick.setX((byte) millis() / 100); // scroll X left to right repeatedly
+  DigiJoystick.setY((byte) 0x30);
+  DigiJoystick.setXROT((byte) 0x60);
+  DigiJoystick.setYROT((byte) 0x90);
+  DigiJoystick.setZROT((byte) 0xB0);
+  DigiJoystick.setSLIDER((byte) 0xF0);
   
-  //actually set the data values
-  DigiJoystick.setValues( (myBuf));
-  //{X, Y, XROT, YROT, ZROT, SLIDER, buttonLowByte, buttonHighByte}
+  // it's best to use DigiJoystick.delay() because it knows how to talk to
+  // the connected computer - otherwise the USB link can crash with the 
+  // regular arduino delay() function
+  DigiJoystick.delay(50); // wait 50 milliseconds
   
-  //we can also set values like this
-  DigiJoystick.setX((unsigned char)(count>>8));
-  DigiJoystick.setY((unsigned char)0x30);
-  DigiJoystick.setXROT((unsigned char)0x60);
-  DigiJoystick.setYROT((unsigned char)0x90);
-  DigiJoystick.setZROT((unsigned char)0xB0);
-  DigiJoystick.setSLIDER((unsigned char)0xF0);
-  
-  //we can also set buttons like this (lowByte, highByte)
+  // we can also set buttons like this (lowByte, highByte)
   //DigiJoystick.setButtons(0x00, 0x00);
-
 }
