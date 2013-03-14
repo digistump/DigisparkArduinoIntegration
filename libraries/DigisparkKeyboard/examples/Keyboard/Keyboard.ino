a4dd9f65b7289935d561e188e2e21dab5e62017f
Bluebie <a@creativepony.com>
2013-03-14 18:32:40 +1100

    DigiKeyboard: Added print/println stuff so you can easily type in strings and numbers and bits like that. scancode-ascii-table could be compressed better, it has a bunch of zeros in it currently, all near the start!

--- libraries/DigisparkKeyboard/examples/Keyboard/Keyboard.ino
+++ libraries/DigisparkKeyboard/examples/Keyboard/Keyboard.ino
@@ -6,33 +6,16 @@ void setup() {
 
 
 void loop() {
-  // running DigiKeyboard.update() from time to time is a good idea - it talks to the computer, answering any
-  // requests the computer has made recently. If you use DigiKeyboard.delay() plentifully you can omit this
-  // as DigiKeyboard.delay() calls update internally.
-  DigiKeyboard.update();
-
-  // this is generally not necessary but with some older systems it seems to prevent missing the first character after a delay:
+  // this is generally not necessary but with some older systems it seems to
+  // prevent missing the first character after a delay:
   DigiKeyboard.sendKeyStroke(0);
-
-  // send out some keys
-  DigiKeyboard.sendKeyStroke(KEY_H, MOD_SHIFT_RIGHT);
-  DigiKeyboard.sendKeyStroke(KEY_E);
-  DigiKeyboard.sendKeyStroke(KEY_L);
-  DigiKeyboard.sendKeyStroke(KEY_L);
-  DigiKeyboard.sendKeyStroke(KEY_O);
-  DigiKeyboard.sendKeyStroke(KEY_SPACE);
-  DigiKeyboard.sendKeyStroke(KEY_D);
-  DigiKeyboard.sendKeyStroke(KEY_I);
-  DigiKeyboard.sendKeyStroke(KEY_G);
-  DigiKeyboard.sendKeyStroke(KEY_I);
-  DigiKeyboard.sendKeyStroke(KEY_S);
-  DigiKeyboard.sendKeyStroke(KEY_P);
-  DigiKeyboard.sendKeyStroke(KEY_A);
-  DigiKeyboard.sendKeyStroke(KEY_R);
-  DigiKeyboard.sendKeyStroke(KEY_K);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-
-  // It's better to use DigiKeyboard.delay() over the regular Arduino delay() because it keeps
-  // talking to the computer to make sure the computer knows the keyboard is alive and connected
+  
+  // Type out this string letter by letter on the computer (assumes US-style
+  // keyboard)
+  DigiKeyboard.println("Hello Digispark!");
+  
+  // It's better to use DigiKeyboard.delay() over the regular Arduino delay()
+  // if doing keyboard stuff because it keeps talking to the computer to make
+  // sure the computer knows the keyboard is alive and connected
   DigiKeyboard.delay(5000);
 }
